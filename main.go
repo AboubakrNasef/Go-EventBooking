480eed1d0f2ad08c3281360a5557ca22c468fc50
Abobakr Nasef <nasefabobakr725@gmail.com>
2025-08-17 13:25:25 +0300

    add inmemory events

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"net/http"
 
+	"eventBook.sample.com/models"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	engine := gin.Default()
+	server := gin.Default()
 
-	engine.GET("/ping", getEvents)
+	server.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Welcome to the Event Booking API")
+	})
 
-	engine.Run(":8080")
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+
 }
 
 func getEvents(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"events": []string{"event1", "event2", "event3"},
-	})
+	events := models.GetAllEvents() // This should be replaced with actual event fetching logic
+	c.JSON(http.StatusOK, events)
+}
+func createEvent(c *gin.Context) {
+	var newEvent models.Event
+	err := c.ShouldBindJSON(&newEvent)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	newEvent.ID = 1
+	newEvent.UserID = 1
+	c.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": newEvent})
 }
